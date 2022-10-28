f8d22498a1b3c04d614ec9bf5368fc17031a161c
kingdragox99 <kingdragox99@gmail.com>
2022-10-28 22:47:21 +0200

    fix

--- app/src/components/FooterComponents.vue
+++ app/src/components/FooterComponents.vue
@@ -11,13 +11,13 @@
       <p
         class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
       >
-        © 2022 Joyce Huberty —
+        Made by Arthur Lefevre —
         <a
-          href="https://instagram.com/joycehbty"
+          href="https://www.linkedin.com/in/arthur-lefevre/"
           class="text-gray-600 ml-1 hover:text-gray-900"
           rel="noopener noreferrer"
           target="_blank"
-          >@joycehbty</a
+          >linkedin</a
         >
       </p>
       <span
--- app/src/components/SpinnerComponents.vue
+++ app/src/components/SpinnerComponents.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex items-center justify-center space-x-2">
-    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
-    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
-    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-400"></div>
+    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-500"></div>
+    <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-600"></div>
   </div>
 </template>
 
